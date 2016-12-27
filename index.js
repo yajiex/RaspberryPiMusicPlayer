4ec6a3b6f6e2099218ff0cbfefd68aa44997501c
Yajiex Github <yajiex@outlook.com>
2016-12-27 19:47:00 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
+
+http.createServer((request, response) => {
+    request.on('error', (err) => {
+
+    });
+
+    response.on('error', (err) => {
+
+    });
+
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('index.html', (err, data) => {
+            response.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': data.length,
+            });
+            response.write(data);
+            response.end();
+        });
+    } else if (request.method === 'POST') {
+        let body = '';
+        request.on('data', (data) => {
+            body += data;
+            if (body.length > 1e6) {
+                request.connection.destroy();
+            }
+        });
+
+        request.on('end', () => {
+            const post = qs.parse(body);
+            const musicName = post.name;
+            console.log(musicName);
+            response.statusCode = 200;
+            response.end();
+        });
+    }
+}).listen(8080);
