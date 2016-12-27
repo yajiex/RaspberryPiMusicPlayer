19a504cd0978b9dc87904a3c28b9d8e13add4f0e
Yajiex Github <yajiex@outlook.com>
2016-12-27 19:59:44 +0800

    Modify music name

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ http.createServer((request, response) => {
             if (audio) {
                 audio.kill();
             }
-            audio = player.play('example.mp', (err) => {
+            audio = player.play('guangrong.mp3', (err) => {
                 if (err) {
                     throw err;
                 }
